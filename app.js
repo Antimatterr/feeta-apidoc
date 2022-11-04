a10f4188214c5efe72f37be3456eb59e4b9921e0
sidhant singh <singhsidmail23@gmail.com>
2022-11-04 10:57:28 +0530

    api doc

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+// const apiDataInitialization = require("./scrapeData");
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs')
+
+const swaggerJsDocs = YAML.load('./api.yaml')
+
+const morgan = require('morgan')
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
+
+app.use(morgan('combined'));
+
+
+app.use('/api', routes);
+
+const portNo = 8000
+app.listen(portNo, () => {
+  console.log(`server running on ${portNo}`);
+})
